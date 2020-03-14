57e5a93dac55399a7f0c755e5b47cb9ee7d02085
Jess <maochengjie1988@gmail.com>
2020-03-14 14:26:21 +0800

    add assets

---
+++ precache-manifest.bfb59ce00f1b008c2662abf4c3911724.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc9e0a055ed806c0cc5bb09f139856f",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/index.html"
+  },
+  {
+    "revision": "00ddd75d535a199ebb87",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b262f25a279e4be95fce",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/css/main.26d17bee.chunk.css"
+  },
+  {
+    "revision": "00ddd75d535a199ebb87",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/js/2.db74b991.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/js/2.db74b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b262f25a279e4be95fce",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/js/main.f526cddf.chunk.js"
+  },
+  {
+    "revision": "8b01fae12579972863b7",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/js/runtime-main.e35f49bb.js"
+  },
+  {
+    "revision": "8befb1cae8a7271022e311145231b894",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/media/banner2.8befb1ca.jpg"
+  },
+  {
+    "revision": "bd5f585eea460c107b571002f117ec6b",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/media/fuwuhao.bd5f585e.jpg"
+  },
+  {
+    "revision": "16cbc92400968829392c8e127db3d46e",
+    "url": "https://cdn.jsdelivr.net/gh/sophister/xxz-1/static/media/kefu.16cbc924.jpg"
+  }
+]);
